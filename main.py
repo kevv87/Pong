b1e9e4c56f1c142390ec36dbcc8b4b57960cab3c
kevv87 <7m6JHC84iMsw>
2018-05-05 21:17:46 -0600

    Movimiento de la paleta del jugador

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import pygame
 pygame.init()
 
 white = (255, 255, 255)
+black = (0, 0, 0)
+
+block_height = 20
+block_width = 24
+
+clock = pygame.time.Clock()
 
 class Tablero:
     def __init__(self, E_SCORE, F_SCORE, LEVEL, B_X, B_Y, B_DIRECTION, PC, block_width, block_height):
@@ -12,6 +18,9 @@ class Tablero:
         self.gameDisplay = pygame.display.set_mode((self.width, self.height))
         self.game_matrix = [[]]
         self.matrix_constructor()
+        self.enemy_score = E_SCORE
+        self.friend_score = F_SCORE
+        self.scores()
         self.block_width = block_width
         self.block_height = block_height
         self.FPS = 30
@@ -30,12 +39,7 @@ class Tablero:
         for n in range(25):
             self.game_matrix.append([])
             for m in range(40):
-                if n == 24 or n == 0:
-                    self.game_matrix[n].append(True)
-                elif n%2 == 0 and m == 19:
-                    self.game_matrix[n].append(True)
-                else:
-                    self.game_matrix[n].append(False)
+                self.game_matrix[n].append(False)
         self.game_matrix = self.game_matrix[:len(self.game_matrix)-1]
 
     def get_matrix(self):
@@ -52,8 +56,10 @@ class Tablero:
                 if self.game_matrix[n][m]:
                     pygame.draw.rect(self.gameDisplay,white,[m*self.block_width, n*self.block_height,
                                                              self.block_width, self.block_height])
+                else:
+                    pygame.draw.rect(self.gameDisplay, black, [m * self.block_width, n * self.block_height,
+                                                               self.block_width, self.block_height])
 
-        pygame.display.update()
 
     def score_f(self):
         if self.friend_score == 0:
@@ -119,56 +125,110 @@ class Tablero:
                 for m in range(len(self.game_matrix[0])):
                     if (m == 23 and 2 <= n <= 6) or (n == 2 and 23 <= m <= 25) or (m == 25 and 2 <= n <= 6) or (n == 6 and 23 <= m <= 25):
                         self.game_matrix[n][m] = True
+                    if n == 24 or n == 0:
+                        self.game_matrix[n][m] = True
+                    elif n % 2 == 0 and m == 19:
+                        self.game_matrix[n][m] = True
         elif self.enemy_score == 1:
             for n in range(len(self.game_matrix)):
                 for m in range(len(self.game_matrix[0])):
                     if (m == 23 and 2 <= n <= 6):
+                        self.game_matrix[n][m] = True
+                    if n == 24 or n == 0:
+                        self.game_matrix[n][m] = True
+                    elif n % 2 == 0 and m == 19:
                         self.game_matrix[n][m] = True
         elif self.enemy_score == 2:
             for n in range(len(self.game_matrix)):
                 for m in range(len(self.game_matrix[0])):
                     if ((n == 2 or n == 4 or n == 6) and 23 <= m <= 25) or (m == 25 and n == 3) or (m == 23 and n == 5) :
                         self.game_matrix[n][m] = True
+                    if n == 24 or n == 0:
+                        self.game_matrix[n][m] = True
+                    elif n % 2 == 0 and m == 19:
+                        self.game_matrix[n][m] = True
         elif self.enemy_score == 3:
             for n in range(len(self.game_matrix)):
                 for m in range(len(self.game_matrix[0])):
                     if ((n == 2 or n == 4 or n == 6) and 23 <= m <= 25) or (m == 25 and n == 5) or (m == 25 and n == 3):
+                        self.game_matrix[n][m] = True
+                    if n == 24 or n == 0:
+                        self.game_matrix[n][m] = True
+                    elif n % 2 == 0 and m == 19:
                         self.game_matrix[n][m] = True
         elif self.enemy_score == 4:
             for n in range(len(self.game_matrix)):
                 for m in range(len(self.game_matrix[0])):
                     if (m == 25 and 2 <= n <= 6) or (m == 23 and 2 <= n <= 4) or (m == 24 and n == 4):
                         self.game_matrix[n][m] = True
+                    if n == 24 or n == 0:
+                        self.game_matrix[n][m] = True
+                    elif n % 2 == 0 and m == 19:
+                        self.game_matrix[n][m] = True
         elif self.enemy_score == 5:
             for n in range(len(self.game_matrix)):
                 for m in range(len(self.game_matrix[0])):
                     if ((n == 2 or n == 4 or n == 6) and 23 <= m <= 25) or (m == 25 and n == 5) or (m == 23 and n == 3):
+                        self.game_matrix[n][m] = True
+                    if n == 24 or n == 0:
+                        self.game_matrix[n][m] = True
+                    elif n % 2 == 0 and m == 19:
                         self.game_matrix[n][m] = True
         elif self.enemy_score == 6:
             for n in range(len(self.game_matrix)):
                 for m in range(len(self.game_matrix[0])):
                     if ((n == 2 or n == 4 or n == 6) and 23 <= m <= 25) or (m == 23 and n == 5)or (m == 25 and n == 5) or (m == 23 and n == 3):
                         self.game_matrix[n][m] = True
+                    if n == 24 or n == 0:
+                        self.game_matrix[n][m] = True
+                    elif n % 2 == 0 and m == 19:
+                        self.game_matrix[n][m] = True
         elif self.enemy_score == 7:
             for n in range(len(self.game_matrix)):
                 for m in range(len(self.game_matrix[0])):
                     if (m == 25 and 2 <= n <= 6) or ((m == 24 or m == 23) and n == 2):
+                        self.game_matrix[n][m] = True
+                    if n == 24 or n == 0:
+                        self.game_matrix[n][m] = True
+                    elif n % 2 == 0 and m == 19:
                         self.game_matrix[n][m] = True
         elif self.enemy_score == 8:
             for n in range(len(self.game_matrix)):
                 for m in range(len(self.game_matrix[0])):
                     if ((n == 2 or n == 4 or n == 6) and 23 <= m <= 25) or ((m == 25 or m == 23) and (n == 3 or n == 5)):
                         self.game_matrix[n][m] = True
+                    if n == 24 or n == 0:
+                        self.game_matrix[n][m] = True
+                    elif n % 2 == 0 and m == 19:
+                        self.game_matrix[n][m] = True
         elif self.enemy_score == 9:
             for n in range(len(self.game_matrix)):
                 for m in range(len(self.game_matrix[0])):
                     if (m == 25 and 2 <= n <= 6) or (m == 23 and 2 <= n <= 4) or (m == 24 and n == 4) or (m == 24 and n == 2):
+                        self.game_matrix[n][m] = True
+                    if n == 24 or n == 0:
+                        self.game_matrix[n][m] = True
+                    elif n % 2 == 0 and m == 19:
                         self.game_matrix[n][m] = True
         elif self.enemy_score == 10:
             for n in range(len(self.game_matrix)):
                 for m in range(len(self.game_matrix[0])):
                     if (m == 23 and 2 <= n <= 6) or (m == 25 and 2 <= n <= 6) or (n == 2 and 25 <= m <= 27) or (m == 27 and 2 <= n <= 6) or (n == 6 and 25 <= m <= 27):
                         self.game_matrix[n][m] = True
+                    if n == 24 or n == 0:
+                        self.game_matrix[n][m] = True
+                    elif n % 2 == 0 and m == 19:
+                        self.game_matrix[n][m] = True
+
+    def scores(self):
+        self.score_e()
+        self.score_f()
+
+    def clean_matrix(self):
+        for n in range(len(self.game_matrix)):
+            for m in range(len(self.game_matrix[0])):
+                self.game_matrix[n][m] = False
+        self.scores()
 
     def move_ball(self):
         if self.ball_direction == 'up':
@@ -194,17 +254,13 @@ class Tablero:
 
 
 class Singles(Tablero):
-    def __init__(self, E_SCORE, F_SCORE, LEVEL, B_X, B_Y, B_DIRECTION, PC, P1_Y, P2_Y):
-        Tablero.__init__(self, E_SCORE, F_SCORE, LEVEL, B_X, B_Y, B_DIRECTION, PC)
+    def __init__(self, E_SCORE, F_SCORE, LEVEL, B_X, B_Y, B_DIRECTION, PC, P1_Y, P2_Y, block_height, block_width):
+        Tablero.__init__(self, E_SCORE, F_SCORE, LEVEL, B_X, B_Y, B_DIRECTION, PC, block_height, block_width)
         self.player1_x = 0
         self.player1_y = P1_Y
         self.player2_x = len(self.game_matrix[0])-1
         self.player2_y = P2_Y
 
-        # Ejecutando metodos
-        self.score_f()
-        self.score_e()
-        self.screen()
 
     def move_player1(self, direction):
         if direction == 'down' and self.player1_y + self.paleta_length + 1 != len(self.game_matrix)-1:
@@ -222,7 +278,9 @@ class Singles(Tablero):
 
         self.player2 = Paleta(self.player2_x, self.player2_y)
 
-game_field = Singles(0,0,1,0,0,'str',False,1,1)
+
+game_field = Singles(0, 0, 1, 0, 0, 'str', False, 1, 1, block_height, block_width)
+
 
 class Bola:
     def __init__(self, pos_x, pos_y, block_width, block_height):
@@ -242,11 +300,12 @@ class Bola:
 
 
 class Paleta:
-    def __init__(self, pos_x, pos_y, block_widht, block_height):
-        self.width = block_widht
+    def __init__(self, pos_x, pos_y, block_width, block_height):
+        self.width = block_width
         self.height = block_height
         self.x = pos_x
         self.y = pos_y
+        self.mod_matrix()
 
 
     def mod_matrix(self):
@@ -254,11 +313,45 @@ class Paleta:
         for n in range(len(matrix)):
             for m in range(len(matrix[0])):
                 if m == self.x and n == self.y:
-                    matrix[n][m] = True
+                    for i in range(game_field.paleta_length):
+                        matrix[n+i][m] = True
         game_field.set_matrix(matrix)
 
 
-a = Singles(0, 0, 1, 0, 0, 'str', False, 1, 1)
 
-while True:
-    pass
+def gameloop():
+    game = True
+    player1_x = 0
+    player1_y = 1
+    player1_down_y = False
+    player1_up_y = False
+
+    while game:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    player1_up_y = True
+                elif event.key == pygame.K_DOWN:
+                    player1_down_y = True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    player1_up_y = False
+                elif event.key == pygame.K_DOWN:
+                    player1_down_y = False
+
+        if player1_down_y and player1_y + game_field.paleta_length + 1 < len(game_field.game_matrix):
+            player1_y += 1
+        elif player1_up_y and player1_y+1 > 2:
+            player1_y -= 1
+
+        game_field.clean_matrix()
+        player1 = Paleta(player1_x, player1_y, block_width, block_height)
+        game_field.screen()
+        pygame.display.update()
+
+        clock.tick(30)
+
+
+gameloop()
