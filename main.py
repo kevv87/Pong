a9abd76704f553033f685ffb57298765da61b5cc
kevv87 <7m6JHC84iMsw>
2018-05-26 13:06:23 -0600

    Musica alargada

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ class Tablero:
         if self.level == 1:
             music_file = 'sounds/lvl1.ogg'
         elif self.level == 2:
-            music_file = 'sounds/Shadowblaze-ChampionBattle.ogg'
+            music_file = 'sounds/lvl2.ogg'
         elif self.level == 3:
             music_file = 'sounds/NDY.ogg'
         sample_rate = mutagen.oggvorbis.OggVorbis(music_file).info.sample_rate
