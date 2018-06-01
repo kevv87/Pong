88c06d1679bef505bbd3fd1ea76b7859d0fefbb4
Estebanmn10 <estebanmn1018@estudiantec.cr>
2018-06-01 02:21:39 -0600

    final de pruebas con botones

--- Arduino.py
+++ Arduino.py
@@ -1,10 +1,14 @@
 import pyfirmata
 import time
+import pygame
+import random
+
+pygame.init()
 
 placa = pyfirmata.Arduino('/dev/ttyACM0')
 
 pyfirmata.util.Iterator(placa).start()
-
+'''
 A = placa.get_pin('d:13:o')
 B = placa.get_pin('d:11:o')
 C = placa.get_pin('d:10:o')
@@ -13,7 +17,6 @@ E = placa.get_pin('d:8:o')
 G = placa.get_pin('d:7:o')
 F = placa.get_pin('d:6:o')
 DP = placa.get_pin('d:12:o')
-
 
 def numeros(num):
     if num == 1:
@@ -99,8 +102,40 @@ def numeros(num):
         G.write(False)
         F.write(False)
         DP.write(True)
+'''
 
-for i in range(1, 10):
-    numeros(i)
-    time.sleep(1)
+entrada1 = placa.get_pin('a:0:i')
+entrada1.enable_reporting()
 
+entrada2 = placa.get_pin('d:9:i')
+entrada2.enable_reporting()
+
+entrada3 = placa.get_pin('d:8:i')
+entrada3.enable_reporting()
+
+white = (255,255,255)
+green = (0,255,0)
+
+gameDisplay = pygame.display.set_mode((800,600))
+try:
+    while True:
+        x = random.randint(0,600)
+        y = random.randint(0,400)
+        if entrada1.read() == 1.0:
+            print("HOLA1")
+            pygame.draw.rect(gameDisplay, green, [x, y,
+                                                  20, 24])
+        elif entrada2.read():
+            print("HOLA2")
+            pygame.draw.rect(gameDisplay, white, [x, y,
+                                                  20, 24])
+        elif entrada3.read():
+            print("HOLA3")
+            pygame.draw.rect(gameDisplay, white, [x, y,
+                                                  20, 24])
+        else:
+            pass
+        pygame.display.update()
+        placa.pass_time(0.2)
+finally:
+    placa.exit()
